8c9d4af6e7d92693a06d122b12e2cdc035a50ac9
Katelyn Gadd <kg@luminance.org>
2019-10-03 11:23:32 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ExceptionRewriter {
+    class Program {
+        public static int Main (string[] args) {
+            try {
+                var argv = args.ToList();
+                if (argv.Count != 2) {
+                    Usage();
+                    return 1;
+                }
+
+                Console.WriteLine("Not implemented");
+                return 2;
+            } finally {
+                if (Debugger.IsAttached) {
+                    Console.WriteLine("Press enter to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void Usage () {
+            Console.WriteLine("Expected: exceptionrewriter [input] [output]");
+        }
+    }
+}
