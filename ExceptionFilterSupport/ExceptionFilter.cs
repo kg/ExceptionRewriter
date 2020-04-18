ebaa165549433fafc3be0631eb89c10df84c5c78
Katelyn Gadd <kg@luminance.org>
2020-04-17 20:56:45 -0700

    Rework exception filter implementation to handle multiple exceptions being live simultaneously

--- ExceptionFilterSupport/ExceptionFilter.cs
+++ ExceptionFilterSupport/ExceptionFilter.cs
@@ -4,25 +4,29 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Mono {
+	public class ExceptionFilterException : Exception {
+		public ExceptionFilterException (string message)
+			: base (message) {
+		}
+
+		public ExceptionFilterException (string message, Exception innerException)
+			: base (message, innerException) {
+		}
+	}
+
 	public abstract class ExceptionFilter {
-		private class HasFilterRunTable : Dictionary<ExceptionFilter, bool> {
+		private class ResultTable : Dictionary<object, int> {
 		}
 
 		private class ThreadState {
-			public readonly ConditionalWeakTable<object, HasFilterRunTable> HasFilterRun = 
-				new ConditionalWeakTable<object, HasFilterRunTable> ();
 			public readonly List<ExceptionFilter> ExceptionFilters =
 				new List<ExceptionFilter> (128);
-
-			public object LastEvaluatedException = null;
 		}
+
+		private readonly ResultTable Results = new ResultTable ();
 
 		public static readonly int exception_continue_search = 0;
 		public static readonly int exception_execute_handler = 1;
-
-		public int Result;
-
-		private static readonly object FilterHasRunSentinel = new object();
 
 		private static readonly ThreadLocal<ThreadState> ThreadStates =
 			new ThreadLocal<ThreadState> (() => new ThreadState ());
@@ -30,18 +34,21 @@ namespace Mono {
 		public abstract int Evaluate (object exc);
 
 		public static void Push (ExceptionFilter filter) {
-			filter.Result = exception_continue_search;
 			ThreadStates.Value.ExceptionFilters.Add(filter);
+		}
+
+		private static void ThrowException (string message, Exception innerException = null) {
+			throw new ExceptionFilterException (message, innerException);
 		}
 
 		public static void Pop (ExceptionFilter filter) {
 			var ef = ThreadStates.Value.ExceptionFilters;
 			if (ef.Count == 0)
-				throw new Exception("Corrupt exception filter stack");
+				ThrowException ($"Attempted to pop filter {filter} with empty stack");
 			var current = ef[ef.Count - 1];
 			ef.RemoveAt(ef.Count - 1);
 			if (current != filter)
-				throw new Exception("Corrupt exception filter stack");
+				ThrowException ($"Attempted to pop filter {filter} but current filter on stack was {current}");
 		}
 
 		/// <summary>
@@ -50,15 +57,14 @@ namespace Mono {
 		/// <param name="exc">The exception being filtered.</param>
 		/// <returns>true if this filter selected the exception handler to run</returns>
 		public bool ShouldRunHandler (object exc) {
-			var ts = ThreadStates.Value;
-
 			if (exc == null)
 				throw new ArgumentNullException("exc");
-			if (ts.LastEvaluatedException != exc)
-				throw new ArgumentException ("Passed exception was not evaluated yet");
 
-			var result = Result == exception_execute_handler;
-			return result;
+			int result;
+			if (!Results.TryGetValue (exc, out result))
+				ThrowException ($"Filter {this} has not been evaluated for {exc}");
+
+			return result == exception_execute_handler;
 		}
 
 		/// <summary>
@@ -71,45 +77,25 @@ namespace Mono {
 		public static void PerformEvaluate (object exc) {
 			var ts = ThreadStates.Value;
 
-			// Attempt to avoid running filters multiple times when unwinding.
-			// FIXME: This may not be correct for rethrow
-			if (ts.LastEvaluatedException == exc)
-				return;
-
 			var hasLocatedValidHandler = false;
-
-			// Set in advance in case the filter throws.
-			// These two state variables allow us to early out in the case where Evaluate() is triggered
-			//  in multiple stack frames while unwinding even though filters have already run.
-			ts.LastEvaluatedException = exc;
-
-			var hfrByException = ts.HasFilterRun;
-			HasFilterRunTable hfrt;
-			if (!hfrByException.TryGetValue(exc, out hfrt)) {
-				hfrt = new HasFilterRunTable();
-				hfrByException.Add(exc, hfrt);
-			}
 
 			for (int i = ts.ExceptionFilters.Count - 1; i >= 0; i--) {
 				var filter = ts.ExceptionFilters[i];
-				if (hasLocatedValidHandler) {
-					filter.Result = exception_continue_search;
-					continue;
-				}
+				if (hasLocatedValidHandler)
+					break;
 
-				if (hfrt.ContainsKey(filter))
-					continue;
-
-				// When an exception filter throws on windows netframework, the filter's exception is
-				//  silently discarded and search for an exception handler continues as if it returned false
-				try {
-					var result = filter.Evaluate(exc);
-					hfrt[filter] = result == exception_execute_handler;
-					filter.Result = result;
-					if (result == exception_execute_handler)
-						hasLocatedValidHandler = true;
-				} catch {
-					filter.Result = exception_continue_search;
+				int result;
+				if (!filter.Results.TryGetValue (exc, out result)) {
+					// When an exception filter throws on windows netframework, the filter's exception is
+					//  silently discarded and search for an exception handler continues as if it returned false
+					try {
+						result = filter.Evaluate(exc);
+						filter.Results[exc] = result;
+						if (result == exception_execute_handler)
+							hasLocatedValidHandler = true;
+					} catch {
+						filter.Results[exc] = exception_continue_search;
+					}
 				}
 			}
 		}
